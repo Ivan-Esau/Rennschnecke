3f2e08498bdd5eaa735e4de70b5a5ff2e6b1e0c6
Ivan-Esau <esau.ivan@fh-swf.de>
2023-11-07 19:51:39 +0100

    New Classes and ENUMs

--- src/Rennschnecke/Rennschnecke.java
+++ src/Rennschnecke/Rennschnecke.java
@@ -6,5 +6,35 @@ public class Rennschnecke
     private Schneckenart art;
     private double weg;
 
+    public Rennschnecke(String name, Schneckenart art, double weg)
+    {
+        this.name = name;
+        this.art = art;
+        this.weg = weg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Schneckenart getArt() {
+        return art;
+    }
+
+    public void setArt(Schneckenart art) {
+        this.art = art;
+    }
+
+    public double getWeg() {
+        return weg;
+    }
+
+    public void setWeg(double weg) {
+        this.weg = weg;
+    }
 
 }
